2aefac88bc97727807e93bf25817d7ad3d1adfa2
akhileshyadav865722 <akhileshyadav9321632gmail.com>
2024-05-23 14:16:57 +0530

    add files

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.btn');
+    const display = document.querySelector('.display');
 
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            const value = e.target.value;
+            const displayValue = display.value;
+
+            switch(value) {
+                case '=':
+                    if(displayValue !== '') {
+                        display.value = eval(displayValue);
+                    }
+                    break;
+                case 'C':
+                    display.value = '';
+                    break;
+                default:
+                    display.value += value;
+            }
+        });
+    });
+});
